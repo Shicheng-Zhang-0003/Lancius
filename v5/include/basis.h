aa1229426a306c7c130120bb971fe913222210ba
shicheng-zhang <shichenghome@gmail.com>
2026-06-24 20:09:16 -0600

    24/06/26: v5 of the entire library, hopefully soon tests can begin with training models

---
+++ v5/include/basis.h
@@ -0,0 +1,14 @@
+#ifndef BASIS_UMBRELLA_H
+#define BASIS_UMBRELLA_H
+
+#include "basis/core/error.h"
+#include "basis/stage1_atomic/scalar.h"
+#include "basis/stage2_linear/tensor.h"
+#include "basis/stage3_sequence/sequence.h"
+#include "basis/stage4_learning/learning.h"
+#include "basis/stage4_learning/optim.h"
+#include "basis/stage5_unified/symbolic.h"
+#include "basis/stage5_unified/compiler.h"
+#include "basis/stage5_unified/geometry.h"
+
+#endif
